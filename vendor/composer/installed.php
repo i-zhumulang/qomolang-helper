7d0060471e3984cb88449755b6243f04b19a83d6
i-zhumulang <1065953740@qq.com>
2023-03-22 11:55:18 +0800

    Initial commit

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'qomolang/helper',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => NULL,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'fb4500659d138b9ce4b73ad3a6848a593ca6103c',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'qomolang/helper' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => NULL,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'fb4500659d138b9ce4b73ad3a6848a593ca6103c',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
